a9bf2238259f0b6dde1cd13ff818f7d38b3f8502
Asaad Jaber <asaadmjaber@gmail.com>
2020-12-16 17:26:52 +0000

    Defined expected positive and negative test cases for checking if a string is "palindromeable".

---
+++ Palindromeable/InputViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InputViewController.swift
+//  Palindromeable
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+    }
+    
+    func mapPairs(sorted str: String) -> Bool {
+        return Bool()
+    }
+    
+}
+
--- PalindromeableTests/PalindromeableTests.swift
+++ PalindromeableTests/PalindromeableTests.swift
@@ -2,32 +2,43 @@
 //  PalindromeableTests.swift
 //  PalindromeableTests
 //
-//  Created by Asaad Jaber on 12/16/20.
-//
 
 import XCTest
 @testable import Palindromeable
 
 class PalindromeableTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var inputViewController: InputViewController!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
+        inputViewController = InputViewController()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    let expectedFail = [
+        "",
+        "a",
+        "ab",
+    ]
+    
+    let expectedPass = [
+        "opp",
+        "nnrrssuu",
+        "nnnrrssuu",
+        "nnnnrrssuu"
+    ]
+    
+    func testFail() {
+        for str in expectedFail {
+            XCTAssertFalse(inputViewController.mapPairs(sorted: str), "Did not fail test for: \(str).")
         }
     }
-
+    
+    func testPass() {
+        for str in expectedPass {
+            XCTAssertTrue(inputViewController.mapPairs(sorted: str), "Did not pass test for: \(str).")
+        }
+    }
+    
 }
